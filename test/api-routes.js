3e1e6d1d8096df01042dd6cca79627c48175dfbb
Tyler Walters <tyler@tylerwalters.com>
2014-11-24 09:34:55 -0700

    Commenting out tests until I have time to write them properly

--- test/api-routes.js
+++ test/api-routes.js
@@ -1,37 +1,90 @@
-var should = require('chai').should();
+// var request 	= require('supertest'),
+// 		should 		= require('chai').should(),
+// 		express 	= require('express'),
+// 		mongoose 	= require('mongoose'),
+// 		app				= express();
 
-describe('API Routing', function() {
-	describe('Photos', function () {
-		it('should pass a sample test', function() {
-			'test'.should.equal('test');
-		});
+// require('../api/routes')(app);
 
-		it('should also pass a second sample test', function() {
-			'test2'.should.equal('test2');
-		});
-	});
+// var db = require('./config/db');
 
-	describe('Videos', function () {
-		it('should pass a sample test', function() {
-			'test'.should.equal('test');
-		});
+// describe('API Routing', function() {
+// 	before(function(done) {
+//     mongoose.createConnection(db.url);							
+//     done();
+//   });
 
-		it('should also not pass this test', function() {
-			'test3'.should.equal('test3');
-		});
+// 	describe('Media', function () {
+// 		it('should respond to GET /api/v1/media with all media as JSON', function() {
+// 			request(app)
+// 				.get('/api/v1/media')
+// 				.set('Accept', 'application/json')
+// 				.end(function(err, res) {
+// 					if (err) {
+// 						throw err
+// 					}
 
-		it('should also pass a second sample test', function() {
-			'test2'.should.equal('test2');
-		});
-	});
+// 					res.status.should.equal(200);
+// 					done();
+// 				});
+// 		});
 
-	describe('Users', function () {
-		it('should pass a sample test', function() {
-			'test'.should.equal('test');
-		});
+// 		it('should add new photo to database with call to POST /api/v1/media', function() {
+// 			request(app)
+// 				.post('/api/v1/media')
+// 				.set('Accept', 'application/json')
+// 				.end(function(err, res) {
+// 					if (err) {
+// 						throw err
+// 					}
 
-		it('should also pass a second sample test', function() {
-			'test2'.should.equal('test2');
-		});
-	});
-});
+// 					res.status.should.equal(200);
+// 					done();
+// 				});
+// 		});
+// 	});
+
+// 	describe('Photos', function () {
+// 		it('should respond to GET /api/v1/photos with all photos as JSON', function() {
+// 			request(app)
+// 				.get('/api/v1/photos')
+// 				.set('Accept', 'application/json')
+// 				.end(function(err, res) {
+// 					if (err) {
+// 						throw err
+// 					}
+
+// 					res.status.should.equal(200);
+// 					done();
+// 				});
+// 		});
+
+// 		it('should also pass a second sample test', function() {
+// 			'test2'.should.equal('test2');
+// 		});
+// 	});
+
+// 	describe('Videos', function () {
+// 		it('should pass a sample test', function() {
+// 			'test'.should.equal('test');
+// 		});
+
+// 		it('should also not pass this test', function() {
+// 			'test3'.should.equal('test3');
+// 		});
+
+// 		it('should also pass a second sample test', function() {
+// 			'test2'.should.equal('test2');
+// 		});
+// 	});
+
+// 	describe('Users', function () {
+// 		it('should pass a sample test', function() {
+// 			'test'.should.equal('test');
+// 		});
+
+// 		it('should also pass a second sample test', function() {
+// 			'test2'.should.equal('test2');
+// 		});
+// 	});
+// });
