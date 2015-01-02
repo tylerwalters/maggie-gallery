dd23fb0e953d4227ba11b2c39df8cbcf2b581429
Tyler Walters <tyler@tylerwalters.com>
2015-01-02 09:59:01 -0700

    Aligning dependencies

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // modules =================================================
 
-var express			= require('express');
-var app				= express();
-var mongoose		= require('mongoose');
-var bodyParser		= require('body-parser');
-var methodOverride  = require('method-override');
+var express					= require('express');
+var app							= express();
+var mongoose				= require('mongoose');
+var bodyParser			= require('body-parser');
+var methodOverride	= require('method-override');
 
 // configuration ===========================================
 
