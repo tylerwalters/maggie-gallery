acdb580d38f0fa2ba79a4559ea3e0719f58f475c
Tyler Walters <tyler@tylerwalters.com>
2015-01-02 09:58:36 -0700

    Adding API routes for media

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,25 @@
-var express = require('express');
-var path	= require('path');
+var express					= require('express'),
+		path						= require('path'),
+		RouteController = require('./controllers/routes');
+		// AuthController 	= require('./controllers/auth');
 
 module.exports = function(app) {
 	var router = express.Router();
 	// API routes ===========================================================
 
-	
+	router.get('/', function(req, res) {
+		res.json({ message: 'Welcome to the API!' });
+	});
+
+	// API route handlers for /posts/articles
+	router.route('/media')
+		.post(RouteController.postMedia)
+		.get(RouteController.getMedia)
+		.put(RouteController.putMedia);
+		// .delete(RouteController.deleteMedia);
+
+	/* Register API Routes */
+	app.use('/api/v1', router);
 
 	// frontend routes ===========================================================
 
