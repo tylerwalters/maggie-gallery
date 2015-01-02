e4bb92b3fd835447bdfc484c71fe2cfd8dedca6b
Tyler Walters <tyler@tylerwalters.com>
2015-01-02 09:59:23 -0700

    Updating tests for API routes

--- test/api-routes.js
+++ test/api-routes.js
@@ -1,90 +1,70 @@
-// var request 	= require('supertest'),
-// 		should 		= require('chai').should(),
-// 		express 	= require('express'),
-// 		mongoose 	= require('mongoose'),
-// 		app				= express();
+var request 	= require('supertest'),
+		should 		= require('chai').should(),
+		express 	= require('express'),
+		mongoose 	= require('mongoose'),
+		app				= express();
 
-// require('../api/routes')(app);
+require('../api/routes')(app);
 
-// var db = require('./config/db');
+var db = require('./config/db');
 
-// describe('API Routing', function() {
-// 	before(function(done) {
-//     mongoose.createConnection(db.url);							
-//     done();
-//   });
+describe('API Routing', function() {
+	before(function(done) {
+    mongoose.createConnection(db.url);							
+    done();
+  });
 
-// 	describe('Media', function () {
-// 		it('should respond to GET /api/v1/media with all media as JSON', function() {
-// 			request(app)
-// 				.get('/api/v1/media')
-// 				.set('Accept', 'application/json')
-// 				.end(function(err, res) {
-// 					if (err) {
-// 						throw err
-// 					}
+	describe('Media', function () {
+		it('should respond to GET /api/v1/media with all media as JSON', function() {
+			request(app)
+				.get('/api/v1/media')
+				.set('Accept', 'application/json')
+				.end(function(err, res) {
+					if (err) {
+						throw err
+					}
 
-// 					res.status.should.equal(200);
-// 					done();
-// 				});
-// 		});
+					res.status.should.equal(200);
+					done();
+				});
+		});
 
-// 		it('should add new photo to database with call to POST /api/v1/media', function() {
-// 			request(app)
-// 				.post('/api/v1/media')
-// 				.set('Accept', 'application/json')
-// 				.end(function(err, res) {
-// 					if (err) {
-// 						throw err
-// 					}
+		it('should add new photo to database with call to POST /api/v1/media', function() {
+			request(app)
+				.post('/api/v1/media')
+				.set('Accept', 'application/json')
+				.end(function(err, res) {
+					if (err) {
+						throw err
+					}
 
-// 					res.status.should.equal(200);
-// 					done();
-// 				});
-// 		});
-// 	});
+					res.status.should.equal(200);
+					done();
+				});
+		});
+	});
 
-// 	describe('Photos', function () {
-// 		it('should respond to GET /api/v1/photos with all photos as JSON', function() {
-// 			request(app)
-// 				.get('/api/v1/photos')
-// 				.set('Accept', 'application/json')
-// 				.end(function(err, res) {
-// 					if (err) {
-// 						throw err
-// 					}
+	describe('Photos', function () {
+		it('should respond to GET /api/v1/photos with all photos as JSON', function() {
+			request(app)
+				.get('/api/v1/photos')
+				.set('Accept', 'application/json')
+				.end(function(err, res) {
+					if (err) {
+						throw err
+					}
 
-// 					res.status.should.equal(200);
-// 					done();
-// 				});
-// 		});
+					res.status.should.equal(200);
+					done();
+				});
+		});
+	});
 
-// 		it('should also pass a second sample test', function() {
-// 			'test2'.should.equal('test2');
-// 		});
-// 	});
+	describe('Videos', function () {
 
-// 	describe('Videos', function () {
-// 		it('should pass a sample test', function() {
-// 			'test'.should.equal('test');
-// 		});
+	});
 
-// 		it('should also not pass this test', function() {
-// 			'test3'.should.equal('test3');
-// 		});
+	describe('Users', function () {
 
-// 		it('should also pass a second sample test', function() {
-// 			'test2'.should.equal('test2');
-// 		});
-// 	});
-
-// 	describe('Users', function () {
-// 		it('should pass a sample test', function() {
-// 			'test'.should.equal('test');
-// 		});
-
-// 		it('should also pass a second sample test', function() {
-// 			'test2'.should.equal('test2');
-// 		});
-// 	});
-// });
+	});
+});
