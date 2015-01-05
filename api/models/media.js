f2e1101f9a06d592c61d843d59aef5b222c64f7a
Tyler Walters <tyler@tylerwalters.com>
2015-01-05 10:28:50 -0700

    Adding extension to schema

--- api/models/media.js
+++ api/models/media.js
@@ -4,11 +4,12 @@ var Schema		= mongoose.Schema;
 var MediaSchema = new Schema({
 	title: { type: String, required: true },				// Photo or image title
 	type: { type: String, required: true },					// Photo or Video
-	tags: Array,																		// Smiling, Sleeping, Disneyland, etc
+	tags: { type: Array },																		// Smiling, Sleeping, Disneyland, etc
 	pubdate: { type: Date, default: Date.now },			// Initial publish date
 	editdate: { type: Date, default: Date.now },		// Date of last update
 	filename: { type: String, required: true },			// Photo or video filename
-	description: { type: String, required: true }		// Description of photo or video
+	extension: { type: Array, required: true },		// jpg, png, etc
+	description: { type: String}		// Description of photo or video
 });
 
 module.exports = mongoose.model('Media', MediaSchema);
