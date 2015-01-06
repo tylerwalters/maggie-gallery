01f4ca45e96778c67270c62af3ea41cfa16dca6c
Tyler Walters <tyler@tylerwalters.com>
2015-01-06 08:56:45 -0700

    Correcting names of some methods, adding method to GET a single file

--- api/controllers/routes.js
+++ api/controllers/routes.js
@@ -34,33 +34,43 @@ exports.getMedia = function(req, res) {
 	});
 };
 
-/* API PUT endpoint for /api/v1/media/:file */
-exports.putMedia = function(req, res) {
-	Media.findOne({'filename' : req.params.file}, function(err, media) {
+/* API GET endpoint for /api/v1/media/:file */
+exports.getFile = function(req, res) {
+	Media.findOne({'filename' : req.params.file}, function(err, file) {
 		if (err)
 			res.send(err);
 
-		media.title				= req.body.title;
-		media.type				= req.body.type;
-		media.tags				= req.body.tags;
-		media.pubdate			= req.body.pubdate;
-		media.editdate		= req.body.editdate;
-		media.filename		= req.body.filename;
-		media.extension		= req.body.extension;
-		media.description	= req.body.description;
+		res.json(file);
+	});
+};
 
-		media.save(function(err) {
+/* API PUT endpoint for /api/v1/media/:file */
+exports.putFile = function(req, res) {
+	Media.findOne({'filename' : req.params.file}, function(err, file) {
+		if (err)
+			res.send(err);
+
+		file.title				= req.body.title;
+		file.type					= req.body.type;
+		file.tags					= req.body.tags;
+		file.pubdate			= req.body.pubdate;
+		file.editdate			= req.body.editdate;
+		file.filename			= req.body.filename;
+		file.extension		= req.body.extension;
+		file.description	= req.body.description;
+
+		file.save(function(err) {
 			if (err)
 				res.send(err);
 
-			res.json({ message: 'File updated!', data: media });
+			res.json({ message: 'File updated!', data: file });
 		});
 	});
 };
 
 /* API DELETE endpoint for /api/v1/media/:file */
-exports.deleteMedia = function(req, res) {
-	Media.remove({'filename' : req.params.file}, function(err, media) {
+exports.deleteFile = function(req, res) {
+	Media.remove({'filename' : req.params.file}, function(err, file) {
 		if (err)
 			res.send(err);
 
