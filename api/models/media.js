d122b8edf0ac0ad5ec14f7aca60511b82536782b
Tyler Walters <tyler@tylerwalters.com>
2015-01-13 09:02:09 -0700

    Adding dimensions, width, and height to schema

--- api/models/media.js
+++ api/models/media.js
@@ -2,14 +2,17 @@ var mongoose	= require('mongoose');
 var Schema		= mongoose.Schema;
 
 var MediaSchema = new Schema({
-	title: { type: String, required: true },				// Photo or image title
-	type: { type: String, required: true },					// Photo or Video
-	tags: { type: Array },																		// Smiling, Sleeping, Disneyland, etc
-	pubdate: { type: Date, default: Date.now },			// Initial publish date
-	editdate: { type: Date, default: Date.now },		// Date of last update
-	filename: { type: String, required: true },			// Photo or video filename
-	extension: { type: Array, required: true },		// jpg, png, etc
-	description: { type: String}		// Description of photo or video
+	title: {type: String, required: true },			// Photo or image title
+	type: {type: String, required: true },			// Photo or Video
+	tags: {type: Array },												// Smiling, Sleeping, Disneyland, etc
+	pubdate: {type: Date, default: Date.now },	// Initial publish date
+	editdate: {type: Date, default: Date.now },	// Date of last update
+	filename: {type: String, required: true },	// Photo or video filename
+	extension: {type: Array, required: true },	// jpg, png, etc
+	description: {type: String},								// Description of photo or video
+	dimensions: {type: String},									// Width by height dimensions in pixels
+	width: {type: Number},											// Width in pixels
+	height: {type: Number}											// Height in pixels
 });
 
 module.exports = mongoose.model('Media', MediaSchema);
