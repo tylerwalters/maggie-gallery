7adea590cbf1c5be7fb468a0e1a7acda5f2069aa
Tyler Walters <tyler@tylerwalters.com>
2015-01-21 09:49:14 -0700

    Adding date and layout to schema

--- api/models/media.js
+++ api/models/media.js
@@ -5,6 +5,7 @@ var MediaSchema = new Schema({
 	title: {type: String, required: true },			// Photo or image title
 	type: {type: String, required: true },			// Photo or Video
 	tags: {type: Array },												// Smiling, Sleeping, Disneyland, etc
+	date: {type: Date},													// Date the photo was taken
 	pubdate: {type: Date, default: Date.now },	// Initial publish date
 	editdate: {type: Date, default: Date.now },	// Date of last update
 	filename: {type: String, required: true },	// Photo or video filename
@@ -12,7 +13,8 @@ var MediaSchema = new Schema({
 	description: {type: String},								// Description of photo or video
 	dimensions: {type: String},									// Width by height dimensions in pixels
 	width: {type: Number},											// Width in pixels
-	height: {type: Number}											// Height in pixels
+	height: {type: Number},											// Height in pixels
+	layout: {type: String}											// Landscape or Portrait
 });
 
 module.exports = mongoose.model('Media', MediaSchema);
