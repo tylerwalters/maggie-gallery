1195932cab3125f3f6772fa74a93c87b0e489b7c
Tyler Walters <tyler@tylerwalters.com>
2015-01-22 09:12:05 -0700

    Adding controller to process images and make them web ready

---
+++ api/controllers/media.js
@@ -0,0 +1,8 @@
+/**
+ *
+ *	Media Controller
+ *	These methods are for processing images and making them web-ready. The
+ *	processed images should be resized, optimized for the web, and renamed
+ *	to names the website will use.
+ *
+ **/
