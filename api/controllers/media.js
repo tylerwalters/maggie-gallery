b3247847a44a9de050e0af72719c5396d95875de
Tyler Walters <tyler@tylerwalters.com>
2015-02-04 08:52:34 -0700

    Adding methods for optimizing images and deleting media files

--- api/controllers/media.js
+++ api/controllers/media.js
@@ -7,8 +7,95 @@
  *
  **/
 
-var fs = require('fs');
+var fs = require('fs'),
+		gm = require('gm');
 
 module.exports = {
+	imageMobile: function (imagePath, destPath, callback) {
+		callback = callback || function(err) {
+			if (err)
+				throw err;
 
+			console.log('Mobile image created: ' + destPath);
+		};
+
+		gm(imagePath)
+			.resize(320)
+			.autoOrient()
+			.noProfile()
+			.write(destPath, callback);
+	},
+
+	imageDesktop: function (imagePath, destPath, callback) {
+		callback = callback || function(err) {
+			if (err)
+				throw err;
+
+			console.log('Desktop image created: ' + destPath);
+		};
+
+		gm(imagePath)
+			.resize(600, 700)
+			.autoOrient()
+			.noProfile()
+			.write(destPath, callback);
+	},
+
+	imageThumb: function (imagePath, destPath, callback) {
+		callback = callback || function(err) {
+			if (err)
+				throw err;
+
+			console.log('Thumbnail image created: ' + destPath);
+		};
+
+		gm(imagePath)
+			.resize(100, 100)
+			.crop(100, 100, 0, 0)
+			.autoOrient()
+			.noProfile()
+			.write(destPath, callback);
+	},
+
+	imageLarge: function (imagePath, destPath, callback) {
+		callback = callback || function(err) {
+			if (err)
+				throw err;
+
+			console.log('Large image created: ' + destPath);
+		};
+
+		gm(imagePath)
+			.resize(1100, 1100)
+			.autoOrient()
+			.noProfile()
+			.write(destPath, callback);
+	},
+
+	imageBackground: function (imagePath, destPath, callback) {
+		callback = callback || function(err) {
+			if (err)
+				throw err;
+
+			console.log('Background image created: ' + destPath);
+		};
+
+		gm(imagePath)
+			.blur(30, 20)
+			.resize(800, 800)
+			.autoOrient()
+			.noProfile()
+			.write(destPath, callback);
+	},
+
+	removeImage: function (imagePath, callback) {
+		callback = callback || function(err) {
+			if (err)
+				throw err;
+
+			console.log('Successfully deleted ' + imagePath);
+		};
+
+		fs.unlink(imagePath, callback);
+	}
 };
