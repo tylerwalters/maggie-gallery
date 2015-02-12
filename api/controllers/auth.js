4c341514fb0e1d1a9b8b145af717b642b2b83510
Tyler Walters <tyler@tylerwalters.com>
2015-02-12 11:07:32 -0700

    Updating documentation for Authentication controller

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,9 +1,8 @@
 /**
  *
  *	Authentication Controller
- *	These methods are for processing images and making them web-ready. The
- *	processed images should be resized, optimized for the web, and renamed
- *	to names the website will use.
+ *	These methods and dependencies process authentication for post, put, and
+ *	delete methods.
  *
  **/
 
