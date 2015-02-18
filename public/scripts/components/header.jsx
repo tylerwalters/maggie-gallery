c250f17d4c18073150dc986449f341a5016c0fe1
Tyler Walters <tyler@tylerwalters.com>
2015-02-18 09:14:45 -0700

    Separating header into individual components

--- public/scripts/components/header.jsx
+++ public/scripts/components/header.jsx
@@ -1,10 +1,34 @@
+var Logo = React.createClass({
+	render: function () {
+		return (
+			<div className="header__logo pure-u-1-4">
+				<img src="../images/logo.png" alt="Maggie Walters Media Gallery Logo" className="header__logo" />
+			</div>
+		)
+	}
+})
+
+var Navigation = React.createClass({
+	render: function () {
+		return (
+			<nav className="header__nav pure-u-3-4">
+				<ul>
+					<li><a href="/">Home</a></li>
+					<li><a href="/about">About</a></li>
+					<li><a href="/donate">Donate</a></li>
+				</ul>
+			</nav>
+		)
+	}
+})
+
 var Header = React.createClass({
 	render: function () {
 		return (
-			<div className="logo">
-				<h1>Hello, World!</h1>
-				<p>This is the header.</p>
-			</div>
+			<header className="header pure-g">
+				<Logo />
+				<Navigation />
+			</header>
 		);
 	}
 });
