ab612cf261850784bb2ed04ae078bbba2a281b31
Tyler Walters <tyler@tylerwalters.com>
2015-02-19 09:01:01 -0700

    Removing incorrectly placed class

--- public/scripts/components/header.jsx
+++ public/scripts/components/header.jsx
@@ -2,7 +2,7 @@ var Logo = React.createClass({
 	render: function () {
 		return (
 			<div className="header__logo pure-u-1-4">
-				<img src="../images/logo.png" alt="Maggie Walters Media Gallery Logo" className="header__logo" />
+				<img src="../images/logo.png" alt="Maggie Walters Media Gallery Logo" />
 			</div>
 		)
 	}
