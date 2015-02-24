ddf20db730f5e07f9e5c99f926780fd3de37d662
Tyler Walters <tyler@tylerwalters.com>
2015-02-24 07:44:51 -0700

    Moving JSX files into their own directory

---
+++ public/scripts/jsx/components/footer.jsx
@@ -0,0 +1,9 @@
+var Footer = React.createClass({
+	render: function (data) {
+		return (
+			<footer className="content pure-g isotope">
+				<p className="pure-u-1-1">Test text</p>
+			</footer>
+		);
+	}
+});
