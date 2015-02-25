8fef6eed063050994df217effd705c51bbbde819
Tyler Walters <tyler@tylerwalters.com>
2015-02-25 09:14:58 -0700

    Updating React class names

--- public/scripts/jsx/components/detail.jsx
+++ public/scripts/jsx/components/detail.jsx
@@ -1,19 +1,31 @@
-var GalleryImage = React.createClass({
+/** @jsx React.DOM */
+
+var DetailImage = React.createClass({
 	render: function () {
 		return (
 			<div className="header__logo pure-u-1-4">
 				<img src="../images/logo.png" alt="Maggie Walters Media Gallery Logo" className="header__logo" />
 			</div>
-		)
+		);
 	}
-})
+});
+
+var DetailDescription = React.createClass({
+	render: function () {
+		return (
+			<div className="header__logo pure-u-1-4">
+				<img src="../images/logo.png" alt="Maggie Walters Media Gallery Logo" className="header__logo" />
+			</div>
+		);
+	}
+});
 
 var Detail = React.createClass({
 	render: function (data) {
 		return (
 			<main className="content pure-g">
 				<DetailImage src=""></DetailImage>
-				<DetailDescription title={} ></DetailDescription>
+				<DetailDescription></DetailDescription>
 			</main>
 		);
 	}
