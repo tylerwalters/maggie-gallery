42f739fe647a1dc99dc804884981a5638d89224a
Tyler Walters <tyler@tylerwalters.com>
2015-02-27 10:20:56 -0700

    Refactoring code to maintain consistent style, add JSDoc style comments

--- api/controllers/media.js
+++ api/controllers/media.js
@@ -1,105 +1,160 @@
-/**
- *
- *	Media Controller
- *	These methods are for processing images and making them web-ready. The
- *	processed images should be resized, optimized for the web, and renamed
- *	to names the website will use.
- *
- **/
-
 var fs = require('fs'),
 		gm = require('gm');
 
-module.exports = {
-	imageMobile: function (imagePath, destPath, callback) {
-		callback = callback || function(err) {
-			if (err)
-				throw err;
+/** 
+	* MediaController contains methods are for processing images and making them 
+	* web-ready. The processed images should be resized, optimized for the web,
+	* and renamed to names the website will use.
+	*
+	* @namespace MediaController
+	*/
 
-			console.log('Mobile image created: ' + destPath);
+(function () {
+	var MediaController;
+
+	MediaController = (function (MediaController) {
+		'use strict';
+
+		/**
+			* Creates images to be used for mobile devices.
+			* 
+			* @param {string} imagePath The path to the full resolution image.
+			* @param {string} destPath The directory where the new image will be placed.
+			* @param {string} filename The image's file name.
+			* @param {string} extension The image's extension.
+			* @param {function} callback Function to run on success.
+			* 
+			* @memberof MediaController
+			* @public
+			*/
+		MediaController.makeImages = function (imagePath, destPath, filename, extension, callback) {
+			callback = callback || function (err) {
+				if (err)
+					throw err;
+
+				console.log('Mobile image created: ' + destPath);
+			};
+
+			MediaController.imageMobile(imagePath, destPath + filename + '.' + 'mob' + '.' + extension, callback);
+			MediaController.imageDesktop(imagePath, destPath + filename + '.' + 'desk' + '.' + extension, callback);
+			MediaController.imageThumb(imagePath, destPath + filename + '.' + 'thumb' + '.' + extension, callback);
+			MediaController.imageLarge(imagePath, destPath + filename + '.' + 'large' + '.' + extension, callback);
+			MediaController.imageBackground(imagePath, destPath + filename + '.' + 'bg' + '.' + extension, callback);
 		};
 
-		gm(imagePath)
-			.resize(320)
-			.autoOrient()
-			.noProfile()
-			.write(destPath, callback);
-	},
+		/**
+			* Deletes previously created images.
+			* 
+			* @param {string} mediaPath The path to the image to be deleted.
+			* @param {function} callback Function to run on success.
+			* 
+			* @memberof MediaController
+			* @public
+			*/
+		MediaController.removeMedia = function (mediaPath, callback) {
+			callback = callback || function (err) {
+				if (err)
+					throw err;
 
-	imageDesktop: function (imagePath, destPath, callback) {
-		callback = callback || function(err) {
-			if (err)
-				throw err;
+				console.log('Successfully deleted image ' + mediaPath);
+			};
 
-			console.log('Desktop image created: ' + destPath);
+			fs.exists(mediaPath, function (exists) {
+				if (exists) {
+					fs.unlink(mediaPath, callback);
+				}
+			});
 		};
 
-		gm(imagePath)
-			.resize(600, 700)
-			.autoOrient()
-			.noProfile()
-			.write(destPath, callback);
-	},
+		/**
+			* Creates images to be used for mobile devices.
+			* 
+			* @param {string} imagePath The path to the full resolution image.
+			* @param {string} destPath The directory where the new image will be placed.
+			* @param {function} callback Function to run on success.
+			* 
+			* @memberof MediaController
+			*/
+		function imageMobile (imagePath, destPath, callback) {
+			gm(imagePath)
+				.resize(320)
+				.autoOrient()
+				.noProfile()
+				.write(destPath, callback);
+		}
 
-	imageThumb: function (imagePath, destPath, callback) {
-		callback = callback || function(err) {
-			if (err)
-				throw err;
+		/**
+			* Creates images to be used for desktop devices.
+			* 
+			* @param {string} imagePath The path to the full resolution image.
+			* @param {string} destPath The directory where the new image will be placed.
+			* @param {function} callback Function to run on success.
+			* 
+			* @memberof MediaController
+			*/
+		function imageDesktop (imagePath, destPath, callback) {
+			gm(imagePath)
+				.resize(600, 700)
+				.autoOrient()
+				.noProfile()
+				.write(destPath, callback);
+		}
 
-			console.log('Thumbnail image created: ' + destPath);
-		};
+		/**
+			* Creates images thumbnails.
+			* 
+			* @param {string} imagePath The path to the full resolution image.
+			* @param {string} destPath The directory where the new image will be placed.
+			* @param {function} callback Function to run on success.
+			* 
+			* @memberof MediaController
+			*/
+		function imageThumb (imagePath, destPath, callback) {
+			gm(imagePath)
+				.resize(100, 100)
+				.crop(100, 100, 0, 0)
+				.autoOrient()
+				.noProfile()
+				.write(destPath, callback);
+		}
 
-		gm(imagePath)
-			.resize(100, 100)
-			.crop(100, 100, 0, 0)
-			.autoOrient()
-			.noProfile()
-			.write(destPath, callback);
-	},
+		/**
+			* Creates large images for detail viewing.
+			* 
+			* @param {string} imagePath The path to the full resolution image.
+			* @param {string} destPath The directory where the new image will be placed.
+			* @param {function} callback Function to run on success.
+			* 
+			* @memberof MediaController
+			*/
+		function imageLarge (imagePath, destPath, callback) {
+			gm(imagePath)
+				.resize(1100, 1100)
+				.autoOrient()
+				.noProfile()
+				.write(destPath, callback);
+		}
 
-	imageLarge: function (imagePath, destPath, callback) {
-		callback = callback || function(err) {
-			if (err)
-				throw err;
+		/**
+			* Creates images to be used for a background on larger devices.
+			* 
+			* @param {string} imagePath The path to the full resolution image.
+			* @param {string} destPath The directory where the new image will be placed.
+			* @param {function} callback Function to run on success.
+			* 
+			* @memberof MediaController
+			*/
+		function imageBackground (imagePath, destPath, callback) {
+			gm(imagePath)
+				.blur(30, 20)
+				.resize(800, 800)
+				.autoOrient()
+				.noProfile()
+				.write(destPath, callback);
+		}
 
-			console.log('Large image created: ' + destPath);
-		};
+		return MediaController;
+	})({});
 
-		gm(imagePath)
-			.resize(1100, 1100)
-			.autoOrient()
-			.noProfile()
-			.write(destPath, callback);
-	},
-
-	imageBackground: function (imagePath, destPath, callback) {
-		callback = callback || function(err) {
-			if (err)
-				throw err;
-
-			console.log('Background image created: ' + destPath);
-		};
-
-		gm(imagePath)
-			.blur(30, 20)
-			.resize(800, 800)
-			.autoOrient()
-			.noProfile()
-			.write(destPath, callback);
-	},
-
-	removeMedia: function (mediaPath, callback) {
-		callback = callback || function(err) {
-			if (err)
-				throw err;
-
-			console.log('Successfully deleted image ' + mediaPath);
-		};
-
-		fs.exists(mediaPath, function (exists) {
-			if (exists) {
-				fs.unlink(mediaPath, callback);
-			}
-		});
-	}
-};
+	module.exports = MediaController;
+})();
