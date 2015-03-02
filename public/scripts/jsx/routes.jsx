0694763898155cc0d1c76d2f3d7192c73e745920
Tyler Walters <tyler@tylerwalters.com>
2015-03-02 13:20:27 -0700

    Adding react-router dependency

--- public/scripts/jsx/routes.jsx
+++ public/scripts/jsx/routes.jsx
@@ -1,27 +1,26 @@
-/** @jsx React.DOM */
+// var data = [
+// 	{filename: "grandparents-day", extension: "jpg", type: "photo", title: "grandparents-day"},
+// 	{filename: "mom-maggie-and-marie", extension: "jpg", type: "photo", title: "mom-maggie-and-marie"}
+// ];
 
-var data = [
-	{filename: "grandparents-day", extension: "jpg", type: "photo", title: "grandparents-day"},
-	{filename: "mom-maggie-and-marie", extension: "jpg", type: "photo", title: "mom-maggie-and-marie"}
-];
+var Router 	= require('react-router'),
+		DefaultRoute = Router.DefaultRoute,
+		Link = Router.Link,
+		Route = Router.Route,
+		RouteHandler = Router.RouteHandler,
+		routes;
 
-// var DefaultRoute = Router.DefaultRoute,
-// 		Link = Router.Link,
-// 		Route = Router.Route,
-// 		RouteHandler = Router.RouteHandler,
-// 		routes;
+routes = (
+	<Route name="app" path="/" handler={App}>
+		<Route name="detail" handler={Detail}/>
+		<Route name="about" handler={About}/>
+		<Route name="donate" handler={Donate}/>
+		<DefaultRoute handler={Gallery}/>
+	</Route>
+)
 
-// routes = (
-// 	<Route name="app" path="/" handler={App}>
-// 		<Route name="detail" handler={Detail}/>
-// 		<Route name="about" handler={About}/>
-// 		<Route name="donate" handler={Donate}/>
-// 		<DefaultRoute handler={Gallery}/>
-// 	</Route>
-// )
+Router.run(routes, function (Handler) {
+	React.render(<Handler/>, document.getElementById('container'))
+});
 
-// Router.run(routes, function (Handler) {
-// 	React.render(<Handler/>, document.getElementById('container'))
-// });
-
-React.render(<App />, document.getElementById('container'));
+// React.render(<App />, document.getElementById('container'));
