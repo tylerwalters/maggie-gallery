4e49b96882ea849ffd737c7b6613cc4f64d95739
Tyler Walters <tyler@tylerwalters.com>
2015-03-02 09:13:04 -0700

    Removing unnecessary components

--- public/scripts/jsx/components/app.jsx
+++ public/scripts/jsx/components/app.jsx
@@ -5,30 +5,9 @@ var App = React.createClass({
 		return (
 			<div class="page">
 				<Header />
-				<RouteHandler />
+				<Gallery />
 				<Footer />
 			</div>
-		);
-	}
-});
-
-var GalleryImage = React.createClass({
-	render: function () {
-		return (
-			<div className="header__logo pure-u-1-4">
-				<img src="../images/logo.png" alt="Maggie Walters Media Gallery Logo" className="header__logo" />
-			</div>
-		);
-	}
-});
-
-var Detail = React.createClass({
-	render: function (data) {
-		return (
-			<main className="content pure-g">
-				<DetailImage src=""></DetailImage>
-				<DetailDescription></DetailDescription>
-			</main>
 		);
 	}
 });
