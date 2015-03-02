dbaed30a93661d4259495f0aef871e29326bce93
Tyler Walters <tyler@tylerwalters.com>
2015-03-02 09:10:11 -0700

    Temporarily commenting out routes for troubleshooting

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -5,21 +5,23 @@ var data = [
 	{filename: "mom-maggie-and-marie", extension: "jpg", type: "photo", title: "mom-maggie-and-marie"}
 ];
 
-var DefaultRoute = Router.DefaultRoute,
-		Link = Router.Link,
-		Route = Router.Route,
-		RouteHandler = Router.RouteHandler,
-		routes;
+// var DefaultRoute = Router.DefaultRoute,
+// 		Link = Router.Link,
+// 		Route = Router.Route,
+// 		RouteHandler = Router.RouteHandler,
+// 		routes;
 
-routes = (
-	React.createElement(Route, {name: "app", path: "/", handler: App}, 
-		React.createElement(Route, {name: "detail", handler: Detail}), 
-		React.createElement(Route, {name: "about", handler: About}), 
-		React.createElement(Route, {name: "donate", handler: Donate}), 
-		React.createElement(DefaultRoute, {handler: Gallery})
-	)
-)
+// routes = (
+// 	<Route name="app" path="/" handler={App}>
+// 		<Route name="detail" handler={Detail}/>
+// 		<Route name="about" handler={About}/>
+// 		<Route name="donate" handler={Donate}/>
+// 		<DefaultRoute handler={Gallery}/>
+// 	</Route>
+// )
 
-Router.run(routes, function (Handler) {
-	React.render(React.createElement(Handler, null), document.getElementById('container'))
-});
+// Router.run(routes, function (Handler) {
+// 	React.render(<Handler/>, document.getElementById('container'))
+// });
+
+React.render(React.createElement(App, null), document.getElementById('container'));
