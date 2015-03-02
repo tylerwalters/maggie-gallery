cecd4fd2519265b0b9c1e5609b079eda1904a4a3
Tyler Walters <tyler@tylerwalters.com>
2015-03-02 09:12:25 -0700

    Temporarily commenting out routes for troubleshooting

--- public/scripts/jsx/routes.jsx
+++ public/scripts/jsx/routes.jsx
@@ -5,21 +5,23 @@ var data = [
 	{filename: "mom-maggie-and-marie", extension: "jpg", type: "photo", title: "mom-maggie-and-marie"}
 ];
 
-var DefaultRoute = Router.DefaultRoute,
-		Link = Router.Link,
-		Route = Router.Route,
-		RouteHandler = Router.RouteHandler,
-		routes;
+// var DefaultRoute = Router.DefaultRoute,
+// 		Link = Router.Link,
+// 		Route = Router.Route,
+// 		RouteHandler = Router.RouteHandler,
+// 		routes;
 
-routes = (
-	<Route name="app" path="/" handler={App}>
-		<Route name="detail" handler={Detail}/>
-		<Route name="about" handler={About}/>
-		<Route name="donate" handler={Donate}/>
-		<DefaultRoute handler={Gallery}/>
-	</Route>
-)
+// routes = (
+// 	<Route name="app" path="/" handler={App}>
+// 		<Route name="detail" handler={Detail}/>
+// 		<Route name="about" handler={About}/>
+// 		<Route name="donate" handler={Donate}/>
+// 		<DefaultRoute handler={Gallery}/>
+// 	</Route>
+// )
 
-Router.run(routes, function (Handler) {
-	React.render(<Handler/>, document.getElementById('container'))
-});
+// Router.run(routes, function (Handler) {
+// 	React.render(<Handler/>, document.getElementById('container'))
+// });
+
+React.render(<App />, document.getElementById('container'));
