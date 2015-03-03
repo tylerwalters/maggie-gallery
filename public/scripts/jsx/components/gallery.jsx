80a022f4ef432ed7cd28f4f25031841e9bd534db
Tyler Walters <tyler@tylerwalters.com>
2015-03-03 16:03:15 -0700

    Add additional image to gallery

--- public/scripts/jsx/components/gallery.jsx
+++ public/scripts/jsx/components/gallery.jsx
@@ -19,6 +19,7 @@ var Gallery = React.createClass({
 					<GalleryImage src="../images/IMAG0745.desk.jpg" title="mom-maggie-and-marie" className="gallery__image"></GalleryImage>
 					<GalleryImage src="../images/IMAG0712.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
 					<GalleryImage src="../images/IMAG0751.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
+					<GalleryImage src="../images/grandparents-day.desk.jpg" title="mom-maggie-and-marie" className="gallery__image gallery__image--w2"></GalleryImage>
 					<GalleryImage src="../images/IMAG0769.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
 					<GalleryImage src="../images/IMAG0770.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
 					<GalleryImage src="../images/IMAG0790.desk.jpg" title="grandparents-day" className="gallery__image gallery__image--w2"></GalleryImage>
@@ -30,8 +31,10 @@ var Gallery = React.createClass({
 					<GalleryImage src="../images/IMAG0720.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
 					<GalleryImage src="../images/IMAG0745.desk.jpg" title="mom-maggie-and-marie" className="gallery__image"></GalleryImage>
 					<GalleryImage src="../images/IMAG0712.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
+					<GalleryImage src="../images/grandparents-day.desk.jpg" title="mom-maggie-and-marie" className="gallery__image gallery__image--w2"></GalleryImage>
 					<GalleryImage src="../images/IMAG0751.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
 					<GalleryImage src="../images/IMAG0769.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
+					<GalleryImage src="../images/IMAG0717.desk.jpg" title="mom-maggie-and-marie" className="gallery__image"></GalleryImage>
 					<GalleryImage src="../images/IMAG0770.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
 					<GalleryImage src="../images/IMAG0790.desk.jpg" title="grandparents-day" className="gallery__image gallery__image--w2"></GalleryImage>
 					<GalleryImage src="../images/IMAG0771.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
