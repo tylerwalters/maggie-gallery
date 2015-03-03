099c45aeef11bb53e2dd710baf0565a9e7f01668
Tyler Walters <tyler@tylerwalters.com>
2015-03-02 19:42:39 -0700

    Adding About and Donate component

---
+++ public/scripts/jsx/components/donate.jsx
@@ -0,0 +1,10 @@
+var About = React.createClass({
+	render: function (data) {
+		return (
+			<main className="content pure-g">
+				<h1>About Maggie</h1>
+				<p>About Maggie.</p>
+			</main>
+		);
+	}
+});
