6bdeab0e3dbd5ed86ff9ca21cb4337c7b9c0c29c
Tyler Walters <tyler@tylerwalters.com>
2015-03-04 09:57:01 -0700

    Adding dependencies, fixing component name

--- build/scripts/jsx/components/routes.jsx
+++ build/scripts/jsx/components/routes.jsx
@@ -3,10 +3,15 @@ var Router 	= require('react-router'),
 		Link = Router.Link,
 		Route = Router.Route,
 		RouteHandler = Router.RouteHandler,
-		routes;
+		routes,
+		Index = require('./index'),
+		Detail = require('./detail'),
+		About = require('./about'),
+		Donate = require('./donate'),
+		Gallery = require('./gallery');
 
 routes = (
-	<Route name="app" path="/" handler={App}>
+	<Route name="index" path="/" handler={Index}>
 		<Route name="detail" handler={Detail}/>
 		<Route name="about" handler={About}/>
 		<Route name="donate" handler={Donate}/>
