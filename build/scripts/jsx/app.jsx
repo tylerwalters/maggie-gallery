64be398cd31debb20aee49fbc6eb0e3d5765363b
Tyler Walters <tyler@tylerwalters.com>
2015-03-04 09:54:21 -0700

    Moving data to component level, fixing dependency typo

--- build/scripts/jsx/app.jsx
+++ build/scripts/jsx/app.jsx
@@ -1,50 +1,3 @@
-var data = {
-	photos: [
-		{
-			filename: 'IMAG0717',
-			extension: 'jpg',
-			title: 'IMAG0717',
-			layout: 'portrait'
-		},
-		{
-			filename: 'mom-maggie-and-marie',
-			extension: 'jpg',
-			title: 'mom-maggie-and-marie',
-			layout: 'landscape'
-		},
-		{
-			filename: 'IMAG0720',
-			extension: 'jpg',
-			title: 'IMAG0720',
-			layout: 'portrait'
-		},
-		{
-			filename: 'IMAG0745',
-			extension: 'jpg',
-			title: 'IMAG0745',
-			layout: 'portrait'
-		},
-		{
-			filename: 'IMAG0712',
-			extension: 'jpg',
-			title: 'IMAG0712',
-			layout: 'portrait'
-		},
-		{
-			filename: 'IMAG0751',
-			extension: 'jpg',
-			title: 'IMAG0751',
-			layout: 'portrait'
-		},
-		{
-			filename: 'grandparents-day',
-			extension: 'jpg',
-			title: 'grandparents-day',
-			layout: 'landscape'
-		}
-	]
-}
-
 var Isotope = require('isotope-layout'),
 		Router 	= require('react-router'),
 		imagesLoaded = require('imagesloaded'),
@@ -54,13 +7,13 @@ var Isotope = require('isotope-layout'),
 		Footer = require('./components/footer'),
 		Gallery = require('./components/gallery'),
 		Header = require('./components/header'),
-		Routes = require('./components/Routes'),
+		Routes = require('./components/routes'),
 		Index = require('./components/index');
 
 
-Router.run(routes, Router.HistoryLocation, function (Handler) {
+Router.run(Routes, Router.HistoryLocation, function (Handler) {
 	var data = data;
-	React.render(<Handler data={data} />, document.body);
+	React.render(<Handler />, document.body);
 });
 
 (function () {
