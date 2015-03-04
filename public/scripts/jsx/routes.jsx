3cec8c7c3068fa185a5e385917e87b21dab73a03
Tyler Walters <tyler@tylerwalters.com>
2015-03-04 09:04:50 -0700

    Adding mock data to test, configuring router to pass data to components

--- public/scripts/jsx/routes.jsx
+++ public/scripts/jsx/routes.jsx
@@ -3,6 +3,53 @@
 // 	{filename: "mom-maggie-and-marie", extension: "jpg", type: "photo", title: "mom-maggie-and-marie"}
 // ];
 
+var data = {
+	photos: [
+		{
+			filename: 'IMAG0717',
+			extension: 'jpg',
+			title: 'IMAG0717',
+			layout: 'portrait'
+		},
+		{
+			filename: 'mom-maggie-and-marie',
+			extension: 'jpg',
+			title: 'mom-maggie-and-marie',
+			layout: 'landscape'
+		},
+		{
+			filename: 'IMAG0720',
+			extension: 'jpg',
+			title: 'IMAG0720',
+			layout: 'portrait'
+		},
+		{
+			filename: 'IMAG0745',
+			extension: 'jpg',
+			title: 'IMAG0745',
+			layout: 'portrait'
+		},
+		{
+			filename: 'IMAG0712',
+			extension: 'jpg',
+			title: 'IMAG0712',
+			layout: 'portrait'
+		},
+		{
+			filename: 'IMAG0751',
+			extension: 'jpg',
+			title: 'IMAG0751',
+			layout: 'portrait'
+		},
+		{
+			filename: 'grandparents-day',
+			extension: 'jpg',
+			title: 'grandparents-day',
+			layout: 'landscape'
+		}
+	]
+}
+
 var Isotope = require('isotope-layout'),
 		Router 	= require('react-router'),
 		imagesLoaded = require('imagesloaded'),
@@ -22,7 +69,7 @@ routes = (
 )
 
 Router.run(routes, Router.HistoryLocation, function (Handler) {
-	React.render(<Handler/>, document.body);
+	React.render(<Handler params={params} data={data} />, document.body);
 });
 
 (function () {
@@ -33,7 +80,7 @@ Router.run(routes, Router.HistoryLocation, function (Handler) {
 		iso = new Isotope(gallery, {
 			itemSelector: '.gallery__image',
 			masonry: {
-				columnWidth: '.gallery__image--grid-set',
+				columnWidth: '.gallery__image--portrait',
 				gutter: 0
 			}
 		});
