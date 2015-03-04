cdfdc1c13df97a15122d2019eac1439e5de59eb3
Tyler Walters <tyler@tylerwalters.com>
2015-03-04 09:06:05 -0700

    Configuring Gallery component to use data passed in rather than hard coded values

--- public/scripts/jsx/components/gallery.jsx
+++ public/scripts/jsx/components/gallery.jsx
@@ -2,44 +2,29 @@ var GalleryImage = React.createClass({
 	render: function () {
 		return (
 			<div className={this.props.className}>
-				<img src={this.props.src} alt={this.props.title} />
+				<a href={this.props.page}><img src={this.props.src} alt={this.props.title} /></a>
 			</div>
 		)
 	}
 });
 
 var Gallery = React.createClass({
-	render: function (data) {
+	render: function () {
+		var imageNodes = this.props.data.photos.map(function (image) {
+			var preparedData = {
+				className: 'gallery__image gallery__image--' + image.layout,
+				page: '/detail/' + image.title,
+				src: '../images/' + image.filename + '.desk.' + image.extension
+			}
+			
+			return (
+				<GalleryImage className={preparedData.className} page={preparedData.page} src={preparedData.src} title={image.title}></GalleryImage>
+			)
+		})
 		return (
 			<main className="content pure-g">
 				<div id="gallery" className="pure-u-1 gallery isotope">
-					<GalleryImage src="../images/IMAG0717.desk.jpg" title="mom-maggie-and-marie" className="gallery__image gallery__image--grid-set"></GalleryImage>
-					<GalleryImage src="../images/mom-maggie-and-marie.desk.jpg" title="mom-maggie-and-marie" className="gallery__image gallery__image--w2"></GalleryImage>
-					<GalleryImage src="../images/IMAG0720.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/IMAG0745.desk.jpg" title="mom-maggie-and-marie" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/IMAG0712.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/IMAG0751.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/grandparents-day.desk.jpg" title="mom-maggie-and-marie" className="gallery__image gallery__image--w2"></GalleryImage>
-					<GalleryImage src="../images/IMAG0769.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/IMAG0770.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/IMAG0790.desk.jpg" title="grandparents-day" className="gallery__image gallery__image--w2"></GalleryImage>
-					<GalleryImage src="../images/IMAG0771.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/2012-07-31_13-18-45_503.desk.jpg" title="grandparents-day" className="gallery__image gallery__image--w2"></GalleryImage>
-					<GalleryImage src="../images/IMAG0736.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/mom-maggie-and-marie.desk.jpg" title="mom-maggie-and-marie" className="gallery__image gallery__image--w2"></GalleryImage>
-					<GalleryImage src="../images/IMAG0717.desk.jpg" title="mom-maggie-and-marie" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/IMAG0720.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/IMAG0745.desk.jpg" title="mom-maggie-and-marie" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/IMAG0712.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/grandparents-day.desk.jpg" title="mom-maggie-and-marie" className="gallery__image gallery__image--w2"></GalleryImage>
-					<GalleryImage src="../images/IMAG0751.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/IMAG0769.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/IMAG0717.desk.jpg" title="mom-maggie-and-marie" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/IMAG0770.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/IMAG0790.desk.jpg" title="grandparents-day" className="gallery__image gallery__image--w2"></GalleryImage>
-					<GalleryImage src="../images/IMAG0771.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
-					<GalleryImage src="../images/2012-07-31_13-18-45_503.desk.jpg" title="grandparents-day" className="gallery__image gallery__image--w2"></GalleryImage>
-					<GalleryImage src="../images/IMAG0736.desk.jpg" title="grandparents-day" className="gallery__image"></GalleryImage>
+					{imageNodes}
 				</div>
 			</main>
 		);
