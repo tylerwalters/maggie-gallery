8d472af28c2919ac858a7619b485b04f00571504
Tyler Walters <tyler@tylerwalters.com>
2015-03-20 15:51:57 -0700

    Updating sortData method to use various sort methods on DataService

--- build/scripts/jsx/pages/home.jsx
+++ build/scripts/jsx/pages/home.jsx
@@ -2,8 +2,25 @@ var DataService = require('../modules/data'),
 		Gallery = require('../components/gallery');
 
 var Home = React.createClass({
-	sortData: function () {
-		this.setState({data: this.state.data.sort()});
+	sortData: function (sortBy) {
+		var sortedData;
+
+		switch (sortBy) {
+			case 'shuffle':
+				sortedData = DataService.sortByShuffle(this.state.data);
+				break;
+			case 'date':
+				sortedData = DataService.sortByDate(this.state.data);
+				break;
+			case 'title':
+				sortedData = DataService.sortByTitle(this.state.data);
+				break;
+			default:
+				sortedData = DataService.sortByShuffle(this.state.data);
+				break;
+		}
+
+		this.setState({data: sortedData});
 	},
 
 	getInitialState: function () {
