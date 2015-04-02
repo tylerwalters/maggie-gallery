118ad936294f9310304bc53ae68a36576b34f996
Tyler Walters <tyler@tylerwalters.com>
2015-04-02 09:52:03 -0700

    Passing url params through react-router

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -2,7 +2,8 @@ var Router 	= require('react-router'),
 		Routes = require('./components/routes');
 
 
-Router.run(Routes, Router.HistoryLocation, function (Handler) {
-	React.render(React.createElement(Handler, null), document.body);
+Router.run(Routes, Router.HistoryLocation, function (Handler, state) {
+	var params = state.params;
+	React.render(React.createElement(Handler, {params: params}), document.body);
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJhcHAuanN4Il0sInNvdXJjZXNDb250ZW50IjpbInZhciBSb3V0ZXIgXHQ9IHJlcXVpcmUoJ3JlYWN0LXJvdXRlcicpLFxuXHRcdFJvdXRlcyA9IHJlcXVpcmUoJy4vY29tcG9uZW50cy9yb3V0ZXMnKTtcblxuXG5Sb3V0ZXIucnVuKFJvdXRlcywgUm91dGVyLkhpc3RvcnlMb2NhdGlvbiwgZnVuY3Rpb24gKEhhbmRsZXIpIHtcblx0UmVhY3QucmVuZGVyKDxIYW5kbGVyIC8+LCBkb2N1bWVudC5ib2R5KTtcbn0pOyJdLCJmaWxlIjoiYXBwLmpzIiwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJhcHAuanN4Il0sInNvdXJjZXNDb250ZW50IjpbInZhciBSb3V0ZXIgXHQ9IHJlcXVpcmUoJ3JlYWN0LXJvdXRlcicpLFxuXHRcdFJvdXRlcyA9IHJlcXVpcmUoJy4vY29tcG9uZW50cy9yb3V0ZXMnKTtcblxuXG5Sb3V0ZXIucnVuKFJvdXRlcywgUm91dGVyLkhpc3RvcnlMb2NhdGlvbiwgZnVuY3Rpb24gKEhhbmRsZXIsIHN0YXRlKSB7XG5cdHZhciBwYXJhbXMgPSBzdGF0ZS5wYXJhbXM7XG5cdFJlYWN0LnJlbmRlcig8SGFuZGxlciBwYXJhbXM9e3BhcmFtc30gLz4sIGRvY3VtZW50LmJvZHkpO1xufSk7Il0sImZpbGUiOiJhcHAuanMiLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
--- build/scripts/jsx/app.jsx
+++ build/scripts/jsx/app.jsx
@@ -2,6 +2,7 @@ var Router 	= require('react-router'),
 		Routes = require('./components/routes');
 
 
-Router.run(Routes, Router.HistoryLocation, function (Handler) {
-	React.render(<Handler />, document.body);
+Router.run(Routes, Router.HistoryLocation, function (Handler, state) {
+	var params = state.params;
+	React.render(<Handler params={params} />, document.body);
 });
