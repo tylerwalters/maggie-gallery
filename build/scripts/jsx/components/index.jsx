bdc906f6e667c7e22c681a056d0dc1015fd7830d
tylerwalters <tyler@tylerwalters.com>
2015-05-03 12:21:30 -0700

    Adding React components for page transitions

--- build/scripts/jsx/components/index.jsx
+++ build/scripts/jsx/components/index.jsx
@@ -1,13 +1,16 @@
 var Router = require('react-router'),
 		Header = require('./header'),
-		Footer = require('./footer');
+		Footer = require('./footer'),
+		TransitionGroup = React.addons.CSSTransitionGroup;
 
 var Index = React.createClass({
 	render: function () {
 		return (
 			<div className="page" ref="page">
 				<Header />
-				<Router.RouteHandler {...this.props}/>
+				<TransitionGroup component="main" transitionName="example">
+					<Router.RouteHandler {...this.props}/>
+				</TransitionGroup>
 				<Footer />
 			</div>
 		);
