747dffdff267cb54fd9a7af898ea95a3aec898d6
tylerwalters <tyler@tylerwalters.com>
2015-06-01 07:58:30 -0700

    Finishing shell script to add media to database that is not already added

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,21 @@ do
 
   if [ "$response" == "null" ]
   then
-    echo "$shortname is not in database."
+    echo "$shortname is not in the database."
+    date=`exiftool -s -s -s -createdate "$file"`
+    extension=`exiftool -s -s -s -filetypeextension "$file"`
+    dimensions=`exiftool -s -s -s -imagesize "$file"`
+    width=`exiftool -s -s -s -imagewidth "$file"`
+    height=`exiftool -s -s -s -imageheight "$file"`
+
+    if [ "$width" -gt "$height" ]
+    then
+      layout="landscape"
+    else
+      layout="portrait"
+    fi
+
+    data="date=$date&description=&dimensions=$dimensions&extension=$extension&filename=$shortname&width=$width&height=$height&layout=$layout&type=photo"
+    curl -d "$data" -i "https://themaggie.gallery/api/v1/photos/$shortname"
   fi
 done
